bcf32476a97344c3c272468dc557c1f3993e7861
William Yao <74448566+William-Yao-2000@users.noreply.github.com>
2021-08-06 13:34:25 +0800

    finished in dce.cpp

---
+++ src/ir/src/asm_arm/optimization.cpp
@@ -0,0 +1,146 @@
+//
+// Created by colors_wind on 2021/8/3.
+//
+
+#include <asm_arm/optimization.h>
+
+void asm_arm::architecture_optimize(asm_arm::Module *module) {
+    for(auto *func : module->functionList) {
+        livenessAnalysis(func);
+        for (auto *bb : func->bList) {
+            ArchitectureOptimizer(bb).process();
+        }
+    }
+
+}
+
+asm_arm::ArchitectureOptimizer::ArchitectureOptimizer(BasicBlock *bb) : bb(bb) {}
+
+asm_arm::InstSet *asm_arm::ArchitectureOptimizer::getUse(Operand *key) {
+    auto iter = use.find(key);
+    if (iter != use.cend() && !iter->second.empty()) {
+        return &iter->second;
+    } return nullptr;
+}
+
+void asm_arm::ArchitectureOptimizer::build() {
+    use.clear();
+    for (auto iter=bb->insts.begin(); iter !=bb->insts.end(); iter++) {
+        auto &inst = *iter;
+        for (auto *n : inst->use) {
+            use[n].insert(inst);
+        }
+        if (inst->op == Inst::Op::MUL || inst->op == Inst::Op::ADD)
+            def[dynamic_cast<BinaryInst*>(inst)->dst] = iter;
+    }
+}
+
+void asm_arm::ArchitectureOptimizer::process() {
+    build();
+    for(auto iter = bb->insts.begin();iter != bb->insts.cend();iter++) {
+        if ((*iter)->nop())
+            continue;
+        tryCombineMLA(iter);
+    }
+}
+
+void asm_arm::ArchitectureOptimizer::tryCombineMLA(asm_arm::InstLinkedList::iterator &iter) {
+    auto &inst = *iter;
+    Inst::Op opType;
+    switch (inst->op) {
+        case Inst::Op::ADD:
+            opType = Inst::Op::MLA;
+            break;
+        case Inst::Op::SUB:
+        case Inst::Op::RSB:
+            opType = Inst::Op::MLS;
+            break;
+        default:
+            return;
+    }
+    if (inst->nop()) return;
+
+    auto validMUL = [this](Operand* node) -> BinaryInst* {
+        auto maybeMULIter = getDef(node);
+        // check if the operand is generated from a InstMUL
+        if (maybeMULIter == bb->insts.cend() || maybeMULIter.operator*()->op != Inst::Op::MUL) return nullptr;
+        auto * instMUL = dynamic_cast<BinaryInst*>(*maybeMULIter);
+        // check if the operand is only use in InstAdd
+        auto * dep = getUse(instMUL->dst);
+        if (!dep) throw std::runtime_error("Use indicates the inst is dead but it actually used in ADD inst.");
+        if (dep->size() != 1) return nullptr;
+        return instMUL;
+    };
+    auto *instADD = dynamic_cast<BinaryInst*>(inst);
+    BinaryInst *instMUL, *instMUL2;
+    if ((instMUL = validMUL(instADD->lhs)) && (inst->op == Inst::Op::ADD || inst->op == Inst::Op::RSB)) {
+        Operand* Rn;
+        if (instADD->rhs->type == Operand::Type::Imm) {
+            Rn = Operand::newVReg();
+            auto * instMOV = new MOVInst(Rn, instADD->rhs);
+            instMOV->bb = this->bb;
+            iter = bb->insts.insert(iter, instMOV);
+            iter++;
+        } else Rn = instADD->rhs;
+        instMUL->mark_nop();
+        // replace
+        *iter = new TernaryInst(opType, instADD->dst, instMUL->lhs, instMUL->rhs, Rn);
+        iter.operator*()->bb = this-> bb;
+    } else if ((instMUL2 = validMUL(instADD->rhs)) && (inst->op == Inst::Op::ADD || inst->op == Inst::Op::SUB)) {
+        instMUL2->mark_nop();
+        // replace
+        *iter = new TernaryInst(opType, instADD->dst, instMUL2->lhs, instMUL2->rhs, instADD->lhs);
+        iter.operator*()->bb = this-> bb;
+    }
+}
+
+
+
+
+void asm_arm::livenessAnalysis(Function *function) {
+    // calculate use and def
+    for (auto &b : function->bList) {
+        b->use.clear();
+        b->def.clear();
+        for (const auto &i:b->insts) {
+            for (auto &u : i->use)
+                if (u->type != Operand::Type::Imm && b->def.find(u) == b->def.end())
+                    b->use.insert(u);
+
+            for (auto &d : i->def)
+                if (d->type != Operand::Type::Imm && b->use.find(d) == b->use.end())
+                    b->def.insert(d);
+        }
+        b->liveIn = b->use;
+        b->liveOut.clear();
+    }
+
+    // calculate liveOut and liveIn
+    bool flag = true; // change flag
+    while (flag) {
+        flag = false;
+        // in reverse topological order
+        for (auto iter = function->bList.rbegin(); iter != function->bList.rend(); iter++) {
+            auto *block = *iter;
+            //  newOut := ∪ liveIn(s), s ∈ succ(n)
+            OperandSet newOut;
+            for (auto &s : block->succ())
+                for (auto &i : s->liveIn)
+                    newOut.insert(i);
+            // liveOut changed <=> liveIn changed
+            if (newOut != block->liveOut) {
+                flag = true;
+                block->liveOut = std::move(newOut);
+                // use(n) ∪ (liveOut(n) − def (n))
+                block->liveIn = block->use;
+                for (const auto &i:block->liveOut) {
+                    if (block->def.find(i) == block->def.end())
+                        block->liveIn.insert(i);
+                }
+            }
+        }
+    }
+    for (const auto &i:function->bList.front()->liveIn)
+        if (i->type == Operand::Type::VReg)
+            throw std::runtime_error("first block should not have any live VReg!");
+}
---
+++ src/ir/src/ast/print.cpp
@@ -0,0 +1,318 @@
+#include <ast/ast.h>
+#include <fstream>
+
+namespace ast {
+    static int count;
+
+    void CompUnit::print(std::ofstream &ofd) {
+        count = 0;
+        ofd << "digraph g{\n";
+        int count_compunit = count;
+        ofd << "\tnode" << count_compunit << "[label=\"CompUnit\"];\n";
+        Decl* enn = nullptr;
+        Function* enn_ = nullptr;
+        for (auto en = entries.begin(); en != entries.end(); en++) {
+            enn = dynamic_cast<Decl*>(*en);
+            if (enn) {
+                ofd << "\tnode" << ++count << "[label=\"Decl\"];\n";
+                ofd << "\tnode" << count_compunit << "->node" << count << ";\n";
+                enn->print(ofd);
+            }
+            else {
+                enn_ = dynamic_cast<Function*>(*en);
+                ofd << "\tnode" << ++count << "[label=\"Function\"];\n";
+                ofd << "\tnode" << count_compunit << "->node" << count << ";\n";
+                enn_->print(ofd);
+            }
+        }
+        ofd << "}\n";
+    }
+
+    void InitVal::print(std::ofstream &ofd) {
+        int count_initval = count;
+        ofd << "\tnode" << count_initval << "[label=\"InitVal\"];\n";
+        if (exp != nullptr) {
+            ofd << "\tnode" << ++count << "[label=\"Exp\"];\n";
+            ofd << "\tnode" << count_initval << "->node" << count << ";\n";
+            exp->print(ofd);
+        }
+        else {
+            for (auto it = vals.cbegin(); it != vals.cend(); it++) {
+                ofd << "\tnode" << ++count << "[label=\"InitVal\"];\n";
+                ofd << "\tnode" << count_initval << "->node" << count << ";\n";
+                (*it)->print(ofd);
+            }
+        }
+    }
+
+    void LVal::print(std::ofstream &ofd) {
+        int count_l = count;
+        ofd << "\tnode" << count_l << "[label=\"LVal\"];\n";
+        ofd << "\tnode" << ++count << "[label=\"" << name << "\"];\n";
+        ofd << "\tnode" << count_l << "->node" << count << ";\n";
+        for (auto it = array_dims.cbegin(); it !=array_dims.cend(); it++) {
+            ofd << "\tnode" << ++count << "[label=\"Exp\"];\n";
+            ofd << "\tnode" << count_l << "->node" << count << ";\n";
+            (*it)->print(ofd);
+        }
+    }
+
+    void Decl::print(std::ofstream &ofd) {
+        int count_decl = count;
+        ofd << "\tnode" << count_decl << "[label=\"Decl\"];\n";
+        if (is_const) {
+            ofd << "\tnode" << ++count << "[label=\"const\"];\n";
+            ofd << "\tnode" << count_decl << "->node" << count << ";\n";
+        }
+        ofd << "\tnode" << ++count << "[label=\"int\"];\n";
+        ofd << "\tnode" << count_decl << "->node" << count << ";\n";
+        ofd << "\tnode" << ++count << "[label=\"" << name << "\"];\n";
+        ofd << "\tnode" << count_decl << "->node" << count << ";\n";
+        if (is_array()) {
+            for (auto it = array_dims.cbegin(); it != array_dims.cend(); it++) {
+                ofd << "\tnode" << ++count << "[label=\"[\"];\n";
+                ofd << "\tnode" << count_decl << "->node" << count << ";\n";
+                ofd << "\tnode" << ++count << "[label=\"Exp\"];\n";
+                ofd << "\tnode" << count_decl << "->node" << count << ";\n";
+                (*it)->print(ofd);
+                ofd << "\tnode" << ++count << "[label=\"]\"];\n";
+                ofd << "\tnode" << count_decl << "->node" << count << ";\n";
+            }
+        }
+        if (initval) {
+            ofd << "\tnode" << ++count << "[label=\"=\"];\n";
+            ofd << "\tnode" << count_decl << "->node" << count << ";\n";
+            ofd << "\tnode" << ++count << "[label=\"InitVal\"];\n";
+            ofd << "\tnode" << count_decl << "->node" << count << ";\n";
+            initval->print(ofd);
+        }
+        else {
+
+        }
+    }
+
+    std::string Exp::op_real() {
+        switch(op) {
+            case Op::MUL:
+                return "*";
+            case Op::DIV:
+                return "/";
+            case Op::EQ:
+                return "==";
+            case Op::LESS_EQ:
+                return "<=";
+            case Op::LESS_THAN:
+                return "<";
+            case Op::MOD:
+                return "%";
+            case Op::GREATER_EQ:
+                return ">=";
+            case Op::GREATER_THAN:
+                return ">";
+            case Op::MINUS:
+                return "-";
+            case Op::PLUS:
+                return "+";
+            case Op::LOGIC_OR:
+                return "||";
+            case Op::LOGIC_NOT:
+                return "!";
+            case Op::INEQ:
+                return "!=";
+            case Op::LOGIC_AND:
+                return "&&";
+            default:
+                return "???";
+        }
+    }
+
+    void Exp::print(std::ofstream &ofd) {
+        int count_exp = count;
+        ofd << "\tnode" << count_exp << "[label=\"Exp\"];\n";
+        switch (op) {
+            case Op::CONST_VAL:
+                ofd << "\tnode" << ++count << "[label=\"" << const_val << "\"];\n";
+                ofd << "\tnode" << count_exp << "->node" << count << ";\n";
+                break;
+            case Op::LVAL:
+                ofd << "\tnode" << ++count << "[label=\"LVal\"];\n";
+                ofd << "\tnode" << count_exp << "->node" << count << ";\n";
+                lval->print(ofd);
+                break;
+            case Op::FuncCall:
+                ofd << "\tnode" << ++count << "[label=\"FuncCall\"];\n";
+                ofd << "\tnode" << count_exp << "->node" << count << ";\n";
+                funccall->print(ofd);
+                break;
+            default:
+                ofd << "\tnode" << ++count << "[label=\"Exp\"];\n";
+                ofd << "\tnode" << count_exp << "->node" << count << ";\n";
+                lhs->print(ofd);
+                ofd << "\tnode" << ++count << "[label=\"" << op_real() << "\"];\n";
+                ofd << "\tnode" << count_exp << "->node" << count << ";\n";
+                ofd << "\tnode" << ++count << "[label=\"Exp\"];\n";
+                ofd << "\tnode" << count_exp << "->node" << count << ";\n";
+                rhs->print(ofd);
+                break;
+        }
+    }
+
+    void Cond::print(std::ofstream &ofd) {
+        int count_cond = count;
+        ofd << "\tnode" << count_cond << "[label=\"Cond\"];\n";
+        ofd << "\tnode" << ++count << "[label=\"Exp\"];\n";
+        ofd << "\tnode" << count_cond << "->node" << count << ";\n";
+        exp->print(ofd);
+    }
+
+    void FuncCall::print(std::ofstream &ofd) {
+        int count_funcall = count;
+        ofd << "\tnode" << count_funcall << "[label=\"FuncCall@" << lineno << "\"];\n";
+        ofd << "\tnode" << ++count << "[label=\"" << name << "\"];\n";
+        ofd << "\tnode" << count_funcall << "->node" << count << ";\n";
+        if (!params.empty()) {
+            for (auto it = params.cbegin(); it != params.cend(); it++) {
+                ofd << "\tnode" << ++count << "[label=\"FuncFParam\"];\n";
+                ofd << "\tnode" << count_funcall << "->node" << count << ";\n";
+                (*it)->print(ofd);
+            }
+        }
+    }
+
+    void FuncFParam::print(std::ofstream &ofd) {
+        int count_fpm = count;
+        ofd << "\tnode" << count_fpm << "[label=\"FuncFParam\"];\n";
+        if (type == Decl::VarType::INT)
+            ofd << "\tnode" << ++count << "[label=\"int\"];\n";
+        else
+            ofd << "\tnode" << ++count << "[label=\"char\"];\n";
+        ofd << "\tnode" << count_fpm << "->node" << count << ";\n";
+        ofd << "\tnode" << ++count << "[label=\"LVal\"];\n";
+        ofd << "\tnode" << count_fpm << "->node" << count << ";\n";
+        signature->print(ofd);
+    }
+
+    void Function::print(std::ofstream &ofd) {
+        // skip external functions
+        if(!block)
+            return;
+        int count_func = count;
+        ofd << "\tnode" << count_func << "[label=\"Function\"];\n";
+        if (type == Type::VOID)
+            ofd << "\tnode" << ++count << "[label=\"void\"];\n";
+        else
+            ofd << "\tnode" << ++count << "[label=\"int\"];\n";
+        ofd << "\tnode" << count_func << "->node" << count << ";\n";
+        ofd << "\tnode" << ++count << "[label=\"" << name << "\"];\n";
+        ofd << "\tnode" << count_func << "->node" << count << ";\n";
+        if (!params.empty()) {
+            for (auto it = params.cbegin(); it != params.cend(); it++) {
+                ofd << "\tnode" << ++count << "[label=\"FuncFParam\"];\n";
+                ofd << "\tnode" << count_func << "->node" << count << ";\n";
+                (*it)->print(ofd);
+            }
+        }
+        ofd << "\tnode" << ++count << "[label=\"Block\"];\n";
+        ofd << "\tnode" << count_func << "->node" << count << ";\n";
+        block->print(ofd);
+    }
+
+    void Block::print(std::ofstream &ofd) {
+        int count_block = count;
+        ofd << "\tnode" << count_block << "[label=\"Block\"];\n";
+        Decl* enn = nullptr;
+        Stmt* enn_ = nullptr;
+        for (auto en = entries.begin(); en != entries.end(); en++) {
+            enn = dynamic_cast<Decl*>(*en);
+            if (enn) {
+                ofd << "\tnode" << ++count << "[label=\"Decl\"];\n";
+                ofd << "\tnode" << count_block << "->node" << count << ";\n";
+                enn->print(ofd);
+            }
+            else {
+                enn_ = dynamic_cast<Stmt*>(*en);
+                ofd << "\tnode" << ++count << "[label=\"Stmt\"];\n";
+                ofd << "\tnode" << count_block << "->node" << count << ";\n";
+                enn_->print(ofd);
+            }
+        }
+    }
+
+    void AssignmentStmt::print(std::ofstream &ofd) {
+        int count_asgn = count;
+        ofd << "\tnode" << count_asgn << "[label=\"Stmt " << loop_deep << "\"];\n";
+        ofd << "\tnode" << ++count << "[label=\"LVal\"];\n";
+        ofd << "\tnode" << count_asgn << "->node" << count << ";\n";
+        lval->print(ofd);
+        ofd << "\tnode" << ++count << "[label=\"=\"];\n";
+        ofd << "\tnode" << count_asgn << "->node" << count << ";\n";
+        ofd << "\tnode" << ++count << "[label=\"Exp\"];\n";
+        ofd << "\tnode" << count_asgn << "->node" << count << ";\n";
+        exp->print(ofd);
+    }
+
+    void EvalStmt::print(std::ofstream &ofd) {
+        if (exp) {
+            ofd << "\tnode" << ++count << "[label=\"Stmt " << loop_deep << "\"];\n";
+            ofd << "\tnode" << ++count << "[label=\"Exp\"];\n";
+            ofd << "\tnode" << count - 1 << "->node" << count << ";\n";
+            exp->print(ofd);
+        }
+    }
+
+    void IfStmt::print(std::ofstream &ofd) {
+        int count_if = count;
+        ofd << "\tnode" << count_if << "[label=\"Stmt " << loop_deep << "\"];\n";
+        ofd << "\tnode" << ++count << "[label=\"if\"];\n";
+        ofd << "\tnode" << count_if << "->node" << count << ";\n";
+        ofd << "\tnode" << ++count << "[label=\"Cond\"];\n";
+        ofd << "\tnode" << count_if << "->node" << count << ";\n";
+        cond->print(ofd);
+        ofd << "\tnode" << ++count << "[label=\"Stmt\"];\n";
+        ofd << "\tnode" << count_if << "->node" << count << ";\n";
+        true_block->print(ofd);
+        if (false_block) {
+            ofd << "\tnode" << ++count << "[label=\"else\"];\n";
+            ofd << "\tnode" << count_if << "->node" << count << ";\n";
+            ofd << "\tnode" << ++count << "[label=\"Stmt\"];\n";
+            ofd << "\tnode" << count_if << "->node" << count << ";\n";
+            false_block->print(ofd);
+        }
+    }
+
+    void WhileStmt::print(std::ofstream &ofd) {
+        int count_while = count;
+        ofd << "\tnode" << count_while << "[label=\"Stmt " << loop_deep << "\"];\n";
+        ofd << "\tnode" << ++count << "[label=\"while\"];\n";
+        ofd << "\tnode" << count_while << "->node" << count << ";\n";
+        ofd << "\tnode" << ++count << "[label=\"Cond\"];\n";
+        ofd << "\tnode" << count_while << "->node" << count << ";\n";
+        cond->print(ofd);
+        ofd << "\tnode" << ++count << "[label=\"Stmt\"];\n";
+        ofd << "\tnode" << count_while << "->node" << count << ";\n";
+        block->print(ofd);
+    }
+
+    void BreakStmt::print(std::ofstream &ofd) {
+        ofd << "\tnode" << count << "[label=\"Stmt " << loop_deep << "\"];\n";
+        ofd << "\tnode" << ++count << "[label=\"break\"];\n";
+        ofd << "\tnode" << count - 1 << "->node" << count << ";\n";
+    }
+
+    void ContinueStmt::print(std::ofstream &ofd) {
+        ofd << "\tnode" << count << "[label=\"Stmt " << loop_deep << "\"];\n";
+        ofd << "\tnode" << ++count << "[label=\"continue\"];\n";
+        ofd << "\tnode" << count - 1 << "->node" << count << ";\n";
+    }
+
+    void ReturnStmt::print(std::ofstream &ofd) {
+        ofd << "\tnode" << count << "[label=\"Stmt " << loop_deep << "\"];\n";
+        ofd << "\tnode" << ++count << "[label=\"return\"];\n";
+        ofd << "\tnode" << count - 1 << "->node" << count << ";\n";
+        if (ret) {
+            ofd << "\tnode" << ++count << "[label=\"Exp\"];\n";
+            ofd << "\tnode" << count - 2 << "->node" << count << ";\n";
+            ret->print(ofd);
+        }
+    }
+}
