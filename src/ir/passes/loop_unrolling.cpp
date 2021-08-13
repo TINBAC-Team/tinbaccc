49ced6ba61d2c1ea0dd4a7a8b9b638717e9ac36c
ColorsWind <14761584+colorswind@users.noreply.github.com>
2021-08-13 14:44:19 +0800

    ir: loop_unrolling: support flexible loop analysis

--- src/ir/passes/loop_unrolling.cpp
+++ src/ir/passes/loop_unrolling.cpp
@@ -31,6 +31,8 @@ struct LoopIR {
     ir::BinaryInst *cmpInst = nullptr;
     ir::BranchInst *branchInst = nullptr;
 
+    LoopVariable *loopCondVar = nullptr;
+
     std::vector<ir::PhiInst *> phiInst;
     std::unordered_map<ir::Value *, LoopVariable *> loopVarsByDefine;
     std::unordered_map<ir::Value *, LoopVariable *> loopVarsByEnter;
@@ -137,8 +139,9 @@ public:
         return true;
     }
 
-    bool constLoopCondAnalysis(ir::Loop *loop, ir::BinaryInst *cmpInst, ir::BranchInst *branchInst, int &loopCount,
-                              int &loopDelta, LoopVariable *loopVar) {
+    bool constLoopCondAnalysis(LoopIR *loopIR, int &loopCount, int &loopDelta) {
+        auto *&cmpInst = loopIR->cmpInst;
+        auto *&branchInst = loopIR->branchInst;
         auto *cmpValueL = dynamic_cast<ir::ConstValue *>(cmpInst->ValueL.value);
         auto *cmpValueR = dynamic_cast<ir::ConstValue *>(cmpInst->ValueR.value);
         loopDelta = 0;
@@ -146,12 +149,15 @@ public:
             // left operator is loopVar and right operator is const
             auto * phiInst = dynamic_cast<ir::PhiInst *>(cmpInst->ValueL.value);
             if (!phiInst) return false;
-            loopVar->init(loop, branchInst, dynamic_cast<ir::PhiInst *>(phiInst));
-            auto *loopVarInit = dynamic_cast<ir::ConstValue *>(loopVar->loopVarInit);
+            auto iter = loopIR->loopVarsByDefine.find(cmpInst->ValueL.value);
+            if (iter == loopIR->loopVarsByDefine.cend())
+                throw std::runtime_error("Cannot find loopCondVar from ValueL of cmpInst.");
+            loopIR->loopCondVar = iter->second;
+            auto *loopVarInit = dynamic_cast<ir::ConstValue *>(loopIR->loopCondVar->loopVarInit);
             if (!loopVarInit) return false;
             int loopInit = loopVarInit->value;
             int condConst = cmpValueR->value;
-            if (tryInferLoopVarDelta(loopVar, branchInst->true_block, loopDelta)
+            if (tryInferLoopVarDelta(loopIR->loopCondVar, branchInst->true_block, loopDelta)
                 && tryInferLoopCount(cmpInst->optype, loopInit, condConst, loopDelta, loopCount)) {
                 return true;
             }
@@ -159,8 +165,12 @@ public:
             // right operator is loopVar and left operator is const
             auto * phiInst = dynamic_cast<ir::PhiInst *>(cmpInst->ValueR.value);
             if (!phiInst) return false;
-            loopVar->init(loop, branchInst, dynamic_cast<ir::PhiInst *>(phiInst));
-            auto *loopVarInit = dynamic_cast<ir::ConstValue *>(loopVar->loopVarInit);
+            auto iter = loopIR->loopVarsByDefine.find(cmpInst->ValueR.value);
+            if (iter == loopIR->loopVarsByDefine.cend())
+                throw std::runtime_error("Cannot find loopCondVar from ValueR of cmpInst.");
+            loopIR->loopCondVar = iter->second;
+            loopIR->loopCondVar->init(loopIR->loop, branchInst, dynamic_cast<ir::PhiInst *>(phiInst));
+            auto *loopVarInit = dynamic_cast<ir::ConstValue *>(loopIR->loopCondVar->loopVarInit);
             if (!loopVarInit) return false;
             int loopInit = loopVarInit->value;
             int condConst = cmpValueL->value;
@@ -183,12 +193,34 @@ public:
                     opType = cmpInst->optype;
                     break;
             }
-            if (tryInferLoopVarDelta(loopVar, branchInst->true_block, loopDelta)
+            if (tryInferLoopVarDelta(loopIR->loopCondVar, branchInst->true_block, loopDelta)
                 && tryInferLoopCount(opType, loopInit, condConst, loopDelta, loopCount)) {
                 return true;
             }
         }
         return false;
+    }
+
+    bool flexibleLoopAnalysis(LoopIR *loopIR, int &loopCount, int &loopDelta) {
+        auto *&cmpInst = loopIR->cmpInst;
+        auto *&branchInst = loopIR->branchInst;
+        auto *cmpValueL = dynamic_cast<ir::PhiInst *>(cmpInst->ValueL.value);
+        auto *cmpValueR = dynamic_cast<ir::PhiInst *>(cmpInst->ValueR.value);
+        loopIR->loopCondVar = new LoopVariable();
+        if (cmpValueL && (!cmpValueR  || !cmpValueR->GetRelatedValue(loopIR->body))) {
+            // left operator is loopVar and right operator will not change in loop body
+            auto iter = loopIR->loopVarsByDefine.find(cmpValueL);
+            if (iter == loopIR->loopVarsByDefine.cend())
+                throw std::runtime_error("Cannot find loopCondVar from cmpValueL.");
+            loopIR->loopCondVar = iter->second;
+            return tryInferLoopVarDelta(loopIR->loopCondVar, branchInst->true_block, loopDelta);
+        } else if (cmpValueR && (!cmpValueL  || !cmpValueL->GetRelatedValue(loopIR->body))) {
+            auto iter = loopIR->loopVarsByDefine.find(cmpValueR);
+            if (iter == loopIR->loopVarsByDefine.cend())
+                throw std::runtime_error("Cannot find loopCondVar from cmpValueR.");
+            loopIR->loopCondVar = iter->second;
+            return tryInferLoopVarDelta(loopIR->loopCondVar, branchInst->true_block, loopDelta);
+        } else return false;
     }
 
     /**
@@ -278,83 +310,110 @@ public:
             }
 
         }
-
     }
 
-    void insertResetLoop(LoopVariable *originLoopVar, LoopIR *originLoopIR, std::vector<ir::Value *> &originInstList,
-                         LoopVariable *unrollingLoopVar) {
+    void fixUnrollingLoopCondition(LoopIR *loopIR, int loopDelta, int loopResetCount) {
+        ir::Use* toFix;
+        if (loopIR->loopCondVar->loopVarDefine == loopIR->cmpInst->ValueL.value) {
+            // left value
+            toFix = &loopIR->cmpInst->ValueR;
+        } else if (loopIR->loopCondVar->loopVarDefine == loopIR->cmpInst->ValueR.value) {
+            toFix = &loopIR->cmpInst->ValueL;
+        } else throw std::runtime_error("neither ValueL nor ValueR is loopCondVar.");
+
+        if (auto x = dynamic_cast<ir::ConstValue*>(toFix->value)) {
+            if (loopDelta > 0) {
+                *toFix = {loopIR->cmpInst, new ir::ConstValue(x->value - loopResetCount)};
+            } else {
+                *toFix = {loopIR->cmpInst, new ir::ConstValue(x->value + loopResetCount)};
+            }
+
+        } else {
+            auto iter = std::find(loopIR->cond->iList.begin(), loopIR->cond->iList.end(), loopIR->cmpInst);
+            ir::Inst* inst;
+            if (loopDelta > 0) {
+                inst = new ir::BinaryInst(ir::OpType::SUB, toFix->value, new ir::ConstValue(K));
+            } else {
+                inst = new ir::BinaryInst(ir::OpType::ADD, toFix->value, new ir::ConstValue(K));
+            }
+            loopIR->cond->iList.insert(iter, inst);
+            toFix->use(inst, true);
+        }
+    }
+
+    void insertResetLoop(LoopIR *resetLoopIR, LoopIR *originLoopIR, std::vector<ir::Value *> &originInstList) {
         // Step1 prepare Loop struct
         auto *reset_loop = new ir::Loop();
         reset_loop->head = originLoopIR->body;
         reset_loop->depth = originLoopIR->loop->depth;
+        auto * originCondVar = originLoopIR->loopCondVar;
 
         // Step2 prepare LoopIR struct
-        LoopIR reset_loopIR;
-        reset_loopIR.loop = reset_loop;
-        reset_loopIR.cond = new ir::BasicBlock("while_unrolling.entry");
-        reset_loopIR.body = new ir::BasicBlock("while_unrolling.true");
+        resetLoopIR->loop = reset_loop;
+        resetLoopIR->cond = new ir::BasicBlock("while_unrolling.entry");
+        resetLoopIR->body = new ir::BasicBlock("while_unrolling.true");
 
         // Step3 prepare phi instructions
         for (auto &originPhiInst : originLoopIR->phiInst) {
             auto *phiInst = new ir::PhiInst();
             phiInst->InsertElem(originLoopIR->cond, originPhiInst);
-            reset_loopIR.phiInst.push_back(phiInst);
-            reset_loopIR.cond->InsertAtEnd(phiInst);
+            resetLoopIR->phiInst.push_back(phiInst);
+            resetLoopIR->cond->InsertAtEnd(phiInst);
         }
 
         // Step4 insert the basic blocks
         auto iter = std::find(function->bList.begin(), function->bList.end(), originLoopIR->body);
         iter++;
-        iter = function->bList.insert(iter, reset_loopIR.cond);
+        iter = function->bList.insert(iter, resetLoopIR->cond);
         iter++;
-        function->bList.insert(iter, reset_loopIR.body);
+        function->bList.insert(iter, resetLoopIR->body);
 
         // Step5 generate IR for reset_loop body
-        duplicateLoopBody(&reset_loopIR, reset_loopIR.body, originLoopIR, originInstList);
-        reset_loopIR.body->InsertAtEnd(new ir::JumpInst(reset_loopIR.cond));
+        duplicateLoopBody(resetLoopIR, resetLoopIR->body, originLoopIR, originInstList);
+        resetLoopIR->body->InsertAtEnd(new ir::JumpInst(resetLoopIR->cond));
 
 
 
         // Step6 generate IR for reset_loop cond
-        LoopVariable *reset_loopVar =
-                reset_loopIR.loopVarsByDefine[reset_loopIR.getCorrespondingPhiInst(originLoopIR, originLoopVar->loopVarDefine)];
+        resetLoopIR->loopCondVar =
+                resetLoopIR->loopVarsByDefine[resetLoopIR->getCorrespondingPhiInst(originLoopIR, originCondVar->loopVarDefine)];
         auto * valueL = originLoopIR->cmpInst->ValueL.value;
         auto * valueR = originLoopIR->cmpInst->ValueR.value;
-        if (valueL == originLoopVar->loopVarDefine) {
-            valueL = reset_loopVar->loopVarDefine;
-        } else if (valueR == originLoopVar->loopVarDefine) {
-            valueR = reset_loopVar->loopVarDefine;
-        } else throw std::runtime_error("One of cmpInst\'s must be originLoopVar.loopVarDefine.");
-        reset_loopIR.cmpInst = new ir::BinaryInst(originLoopIR->cmpInst->optype, valueL, valueR);
-        reset_loopIR.branchInst = new ir::BranchInst(reset_loopIR.cmpInst, reset_loopIR.body,
+        if (valueL == originCondVar->loopVarDefine) {
+            valueL = resetLoopIR->loopCondVar->loopVarDefine;
+        } else if (valueR == originCondVar->loopVarDefine) {
+            valueR = resetLoopIR->loopCondVar->loopVarDefine;
+        } else throw std::runtime_error("One of cmpInst\'s must be originCondVar.loopVarDefine.");
+        resetLoopIR->cmpInst = new ir::BinaryInst(originLoopIR->cmpInst->optype, valueL, valueR);
+        resetLoopIR->branchInst = new ir::BranchInst(resetLoopIR->cmpInst, resetLoopIR->body,
                                                      originLoopIR->branchInst->false_block);
-        reset_loopIR.cond->InsertAtEnd(reset_loopIR.cmpInst);
-        reset_loopIR.cond->InsertAtEnd(reset_loopIR.branchInst);
+        resetLoopIR->cond->InsertAtEnd(resetLoopIR->cmpInst);
+        resetLoopIR->cond->InsertAtEnd(resetLoopIR->branchInst);
 
         // Step6 fill phi instruction
-        for (auto & pair : reset_loopIR.loopVarsByEnter) {
+        for (auto & pair : resetLoopIR->loopVarsByEnter) {
             auto * loopVar = pair.second;
             auto * enterVar = pair.first;
-            loopVar->loopVarDefine->InsertElem(reset_loopIR.branchInst->true_block, enterVar);
+            loopVar->loopVarDefine->InsertElem(resetLoopIR->branchInst->true_block, enterVar);
         }
 
         // Step7 modify origin loop jumpInst
-        originLoopIR->cond->replaceSucc(originLoopIR->branchInst->false_block, reset_loopIR.cond);
+        originLoopIR->cond->replaceSucc(originLoopIR->branchInst->false_block, resetLoopIR->cond);
 
         // Step8 apply changes to loops
         function->deepestLoop.push_back(reset_loop);
         function->loops.insert(reset_loop);
         if (originLoopIR->loop->external)
             originLoopIR->loop->external->nested.push_back(reset_loop);
-        reset_loop->body.insert(reset_loopIR.cond);
-        reset_loop->body.insert(reset_loopIR.body);
+        reset_loop->body.insert(resetLoopIR->cond);
+        reset_loop->body.insert(resetLoopIR->body);
 
         // Step9 replace value
         std::set<ir::BasicBlock*> ignore;
         std::set_union(originLoopIR->loop->body.cbegin(),  originLoopIR->loop->body.cend(),
                        reset_loop->body.cbegin(),  reset_loop->body.cend(), std::inserter(ignore, ignore.cbegin()));
 
-        auto resetIter = reset_loopIR.phiInst.begin();
+        auto resetIter = resetLoopIR->phiInst.begin();
         for (auto * phiInst : originLoopIR->phiInst) {
             std::vector<ir::Use*> usesCopy;
             usesCopy.assign(phiInst->uList.cbegin(), phiInst->uList.cend());
@@ -372,48 +431,48 @@ public:
         if (loop->body.size() != 2) return;
 
         // Step2 Collect necessary information about loop cond
-        LoopVariable loopVar{};
         LoopIR loopIR{};
         loopIR.init(loop);
         loopIR.build();
-        int cnt;
+        int loopCount;
+        int loopDelta;
 
         // Step3 Process loop which execution times can be inferred
-        // TODO Remove loop where cnt = 0
-        int loopDelta;
-        if (constLoopCondAnalysis(loop, loopIR.cmpInst, loopIR.branchInst, cnt, loopDelta, &loopVar) && cnt > 0) {
-            int loopUnrollingCount = cnt / K;
-            int loopResetCount = cnt - loopUnrollingCount * K;
+        // TODO Remove loop where loopCount = 0
+        if (constLoopCondAnalysis(&loopIR, loopCount, loopDelta) && loopCount > 0) {
+            int loopUnrollingCount = loopCount / K;
+            int loopResetCount = loopCount - loopUnrollingCount * K;
             if (loopUnrollingCount == 0) return; // do nothing
             auto *jumpToCond = loopIR.body->iList.back();
             loopIR.body->iList.pop_back();
-            std::vector<ir::Value *> iList{loopIR.body->iList.size()};
+            std::vector<ir::Value *> iList;
             iList.assign(loopIR.body->iList.cbegin(), loopIR.body->iList.cend());
 
-            // reserve last loopVar
-            LoopVariable unrollingLoopVar{};
-            for (int i = 0; i < K - 1; i++) {
-                // loopVarDefine must be filled
-                unrollingLoopVar.loopVarDefine = loopVar.loopVarDefine;
+            for (int i = 0; i < K - 1; i++)
                 duplicateLoopBody(&loopIR, loopIR.body, &loopIR, iList);
-            }
-            std::cout << "unrolling #1" << std::endl;
             loopIR.body->InsertAtEnd(jumpToCond);
-            if (loopResetCount > 0) {
-                insertResetLoop(&loopVar, &loopIR, iList, &unrollingLoopVar);
-                std::cout << "unrolling #2" << std::endl;
-            }
+            std::cout << "unrolling constLoopCond #1" << std::endl;
 
-            if (loopDelta > 0) {
-                loopDelta = -loopResetCount;
-            } else {
-                loopDelta = loopResetCount;
+            if (loopResetCount > 0) {
+                LoopIR resetLoopIR;
+                insertResetLoop(&resetLoopIR, &loopIR, iList);
+                fixUnrollingLoopCondition(&loopIR, loopDelta, loopResetCount);
+                std::cout << "unrolling constLoopCond #2" << std::endl;
             }
-            if (auto x = dynamic_cast<ir::ConstValue*>(loopIR.cmpInst->ValueL.value)) {
-                loopIR.cmpInst->ValueL = {loopIR.cmpInst, new ir::ConstValue(x->value + loopDelta)};
-            } else if (auto x = dynamic_cast<ir::ConstValue*>(loopIR.cmpInst->ValueR.value)) {
-                loopIR.cmpInst->ValueR = {loopIR.cmpInst, new ir::ConstValue(x->value + loopDelta)};
-            }
+        } else if (flexibleLoopAnalysis(&loopIR, loopCount, loopDelta)) {
+            auto *jumpToCond = loopIR.body->iList.back();
+            loopIR.body->iList.pop_back();
+            std::vector<ir::Value *> iList;
+            iList.assign(loopIR.body->iList.cbegin(), loopIR.body->iList.cend());
+            for (int i = 0; i < K - 1; i++)
+                duplicateLoopBody(&loopIR, loopIR.body, &loopIR, iList);
+            loopIR.body->InsertAtEnd(jumpToCond);
+            std::cout << "unrolling flexibleLoopCond #1" << std::endl;
+            LoopIR resetLoopIR;
+            insertResetLoop(&resetLoopIR, &loopIR, iList);
+            // handle two case: 1. the condVarInit is not const; 2. the bound is not const
+            fixUnrollingLoopCondition(&loopIR, loopDelta, K);
+            std::cout << "unrolling flexibleLoopCond #2" << std::endl;
         }
     }
 };
