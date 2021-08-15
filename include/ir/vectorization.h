61785b7aeaaa59b19609fa1c908dd8aa46d36053
ColorsWind <14761584+colorswind@users.noreply.github.com>
2021-08-15 19:37:44 +0800

    ir: vectorization: prepare for codegen

--- include/ir/vectorization.h
+++ include/ir/vectorization.h
@@ -1,14 +1,45 @@
+#include <utility>
+
 #ifndef TINBACCC_VECTORIZATION_H
 #define TINBACCC_VECTORIZATION_H
 namespace ir {
-
-    bool static isVectorization(ir::Value* value);
-
-    class AdjacentMemory {
-        std::vector<ir::Value *> address;
-
+    class VInst;
+    class VLoadInst;
+    class VStoreInst;
+    class IterationAnalyst;
+    class AutoVectorizationContext {
     public:
-        AdjacentMemory(std::vector<ir::Value*> &&address) : address(address){}
+        std::set<IterationAnalyst*> analyst;
+        std::unordered_map<ir::Value*, std::pair<ir::VInst*, int>> associatedVInst;
+
+    };
+
+    class VInst : public virtual Value {
+    public:
+        virtual ir::Value* getAssociatedComponent(int index);
+        virtual int getSize() const;
+    };
+
+    class IterationAnalyst {
+    public:
+        virtual bool analysis(AutoVectorizationContext* context) = 0;
+    };
+
+    class AdjacentMemory : public virtual GetElementPtrInst, public IterationAnalyst {
+    public:
+        std::vector<ir::GetElementPtrInst *> address;
+        AdjacentMemory(std::vector<ir::GetElementPtrInst *> address)
+                : GetElementPtrInst(address.front()->arr.value,
+                                    [address]() {
+                                        std::vector<Value *> dims;
+                                        for (auto &use : address.front()->dims)
+                                            dims.push_back(
+                                                    use.value);
+                                        return dims;
+                                    }(),
+                                    address.front()->multipliers),
+                  address(std::move(address)) {}
+
 
         ir::Value *front() const {
             return address.front();
@@ -17,19 +48,72 @@ namespace ir {
         int length() const {
             return address.size();
         }
-    };
 
-    class VBinaryInst : BinaryInst {
+        int indexOf(ir::Value* value) const {
+            for(int i=0;i< address.size();i++)
+                if (address[i] == value) return i;
+            return -1;
+        }
 
-    };
-
-    class VLoadInst : LoadInst {
-
-    };
-
-    class VStoreInst : StoreInst {
+        bool analysis(AutoVectorizationContext* context);
 
     };
+
+    class VDupInst : public VInst {
+    public:
+        explicit VDupInst(Value* value);
+    };
+
+    class VBinaryInst : public BinaryInst, public IterationAnalyst {
+    public:
+        std::vector<BinaryInst*> associated;
+        VBinaryInst(OpType optype, Value *valueL, Value *valueR, std::vector<BinaryInst*> associated) :
+            BinaryInst(optype, valueL, valueR), associated(std::move(associated)) {}
+
+        bool analysis(AutoVectorizationContext* context);
+
+        static bool convert(BinaryInst *inst) {
+            switch (inst->optype) {
+                case OpType::ADD:
+                case OpType::SUB:
+                case OpType::MUL:
+                case OpType::SDIV:
+                    return true;
+                default:
+                    return false;
+            }
+        }
+
+        static Use & getValue(BinaryInst *binaryInst, bool isLeft) {
+            if (isLeft)
+                return binaryInst->ValueL;
+            else
+                return binaryInst->ValueR;
+        }
+    };
+
+    class VLoadInst : public virtual LoadInst, public virtual VInst, public IterationAnalyst {
+    public:
+        std::vector<LoadInst*> associated;
+        VLoadInst(AdjacentMemory *adj);
+
+        bool analysis(AutoVectorizationContext* context) override;
+
+        int indexOfAssociated(ir::Value* inst) {
+            for(int i=0;i<associated.size();i++)
+                if (associated[i] == inst) return i;
+            return -1;
+        }
+
+    };
+
+    class VStoreInst : public virtual StoreInst, public virtual VInst, public IterationAnalyst {
+        VStoreInst(AdjacentMemory *adj);
+    };
+
+    bool static isVector(ir::Value *value) {
+        return dynamic_cast<VDupInst*>(value) || dynamic_cast<VBinaryInst*>(value) || dynamic_cast<VLoadInst*>(value);
+    }
 
 
 }
